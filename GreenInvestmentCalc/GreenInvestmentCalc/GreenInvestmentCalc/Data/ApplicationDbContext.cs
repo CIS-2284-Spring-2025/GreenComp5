e5409879eb81e9893988f87565f4ab500f00d5cc
dariagrn <daria.green20@gmail.com>
2025-03-24 15:28:22 -0600

    Upload GreenInvestmentCalc to add on to

---
+++ GreenInvestmentCalc/GreenInvestmentCalc/GreenInvestmentCalc/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace GreenInvestmentCalc.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> context) : base(context)
+        {
+
+        }
+    }
+
+    public class MyBlogDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlite("Data Source = data.db");
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
